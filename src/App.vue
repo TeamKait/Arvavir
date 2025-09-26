59c3c44429f47a621cb34d7b7f1cf535d9c696fd
ART <teamkait1@gmail.com>
2025-09-26 21:53:10 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {Button} from "@/components/ui/button";
+import {Icon} from "@iconify/vue";
+import {useColorMode} from "@vueuse/core";
+
+const mode = useColorMode();
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
