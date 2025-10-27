c3c2019bb194a89d7bd475a1eb83fda1cffbfcd9
ART <teamkait1@gmail.com>
2025-10-27 20:16:12 +0300

    bouncy 404

--- src/router.ts
+++ src/router.ts
@@ -38,7 +38,7 @@ const router = createRouter({
         {
             path: '/:pathMatch(.*)*',
             name: 'NotFound',
-            component: () => import('@/views/NotFound.vue')
+            component: () => import('@/views/Page404.vue')
         }
     ],
 })
